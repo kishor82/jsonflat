287018844506deedc31cf286e3984a01e5ab14ef
Kishor Rathva <kishorrathva8298@gmail.com>
2024-04-26 02:48:27 +0530

    refactor(codeql): refactor test based on codeql analysis

--- test/module/esm/index.js
+++ test/module/esm/index.js
@@ -1,6 +1,5 @@
-import flatThisjson , {flatten} from "flatten-this-json";
 import assert from 'assert';
-import flattenThisJson from "flatten-this-json";
+import flattenThisJson, {flatten} from "flatten-this-json";
 
 assert.strictEqual(typeof flattenThisJson.flatten, 'function');
 assert.strictEqual(typeof flatten, 'function');
