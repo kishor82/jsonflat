84cbe5ca6019574353e29f6b95725b825d209fde
Kishor Rathva <kishorrathva8298@gmail.com>
2024-04-27 17:03:11 +0530

    chore(cdn): added unpkg and jsdelivr in package.json
    closes #15

--- test/module/esm/index.js
+++ test/module/esm/index.js
@@ -4,4 +4,4 @@ import flattenThisJson, {flatten} from "flatten-this-json";
 assert.strictEqual(typeof flattenThisJson.flatten, 'function');
 assert.strictEqual(typeof flatten, 'function');
 
-console.log("ESM importing test passed");
+console.log("✅ ESM importing test passed");
